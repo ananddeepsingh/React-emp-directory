0c15c56790dab823049a5cde51c2d43cdd4571fb
ananddeepsingh <singh.ananddeep@gmail.com>
2019-08-04 23:30:45 +0800

    Adding Error Handling

--- src/App.js
+++ src/App.js
@@ -6,67 +6,105 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
+
+    this.URL = 'http://api.additivasia.io/api/v1/assignment/employees/';
+
     this.state = {
       searchUser: '',
       data: [],
       showResult: false,
-      urlTraffic: false
+      errorOccured: false
     }
-    // let params = new URLSearchParams(window.location.search);  
-    // this.dataFromQueryString = params.get('name');
 
-    // if(this.dataFromQueryString && !this.state.urlTraffic){
-    //   this.setState({
-    //     urlTraffic: true
-    //   })
-    //   this.handleSubmit();
-    // }
-  }
+    this.invitedUser = false;
 
-  renderSearch = (e) => {
-    console.log(e.target.value)
+    let params = new URLSearchParams(window.location.search);
+    this.dataFromQueryString = params.get('name');
+
+    if (this.dataFromQueryString !== '' && this.dataFromQueryString) {
+      this.invitedUser = true;
+      this.handleSubmit(null);
+    }
   }
 
   handleSubmit(e) {
-    
-    e.preventDefault();
-    fetch("http://api.additivasia.io/api/v1/assignment/employees/" + (this.input.value || this.dataFromQueryString) )
-      .then((response) => {
-        return response.json();
-      })
-      .then((jsonObj) => {
 
+    let userName;
+
+    if (e) {
+      e.preventDefault();
+    }
+
+    if (this.input && this.input.value === '') {
+      alert('Please Enter Name ')
+      this.input.focus();
+      return false;
+    }
+
+    if (this.invitedUser) {
+      userName = this.dataFromQueryString;
+      this.invitedUser = false;
+    } else {
+      userName = this.input.value;
+    }
+
+    if (userName) {
+      this.renderHTTPCall(userName)
+
+      if (this.input) {
+        this.input.value = '';
+      }
+    }
+  }
+
+  renderHTTPCall(userName) {
+
+    fetch(this.URL + userName)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+
+        throw new Error('Network response was not ok.');
+      }).then((jsonObj) => {
         if (jsonObj.length > 0) {
           this.setState({
-            searchUser: this.input.value,
+            searchUser: userName,
             data: jsonObj,
-            showResult: true
+            showResult: true,
+            errorOccured: false
           });
-        }else{
+        } else {
           this.setState({
             searchUser: '',
             data: [],
-            showResult: false
+            showResult: false,
+            errorOccured: false
           });
         }
-        this.input.value = '';
-      });
+      }).catch(function (error) {
+        this.setState({
+          searchUser: '',
+          errorOccured: true
+        })
+        console.log('There has been a problem with your fetch operation: ', error.message);
+      }.bind(this));
   }
 
-
   render() {
-    
     return (
       <React.Fragment>
+        <h1>WELCOME TO EMPLOYEE SEARCH PORTAL</h1>
         <form onSubmit={(e) => { this.handleSubmit(e) }} type="post">
           <label>
-            <input type="text" ref={(input) => this.input = input} />
+            <input type="text" className="search" placeholder="Please Enter Employee Name" ref={(input) => this.input = input} />
           </label>
-          <input type="submit" value="Search" />
+          <input type="submit" className="searchBtn" value="Search" />
         </form>
 
         {this.state.showResult ? <Results data={this.state} /> : null}
 
+        <div className="error">{this.state.errorOccured ? 'No Record(s) Found' : null} </div>
       </React.Fragment>
     );
   }
--- src/Results.js
+++ src/Results.js
@@ -6,24 +6,22 @@ export default class Results extends React.Component {
     renderSubOrdinates() {
         let results = [];
 
-        this.props.data.data[1]['direct-subordinates'].map((item, i) => {
-            results.push(<SubOrdinates item={item} key={i} />);
-        })
-
-
+        if (this.props.data && this.props.data.data[1].hasOwnProperty('direct-subordinates')) {
+            this.props.data.data[1]['direct-subordinates'].map((item, i) => {
+                results.push(<SubOrdinates item={item} key={i} />);
+            })
+        }
         return results;
     }
 
     render() {
-        console.log(this.props)
         return (
             <div id="result" >
-                <h2>Subordinates of employee - {this.props.data.searchUser}</h2>
-                <div>
-                    <ol start="1">
-                        {this.renderSubOrdinates()}
-                    </ol>
-                </div>
+                <h2>Subordinates of "{this.props.data.searchUser}"</h2>
+                <ol start="1" id='subOrd'>
+                    {this.renderSubOrdinates()}
+                </ol>
+
             </div>
         )
     }
