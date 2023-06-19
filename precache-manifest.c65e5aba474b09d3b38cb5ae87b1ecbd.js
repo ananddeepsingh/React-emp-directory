bce1ced590a7a6391d550f8cfd8c344a73cad417
ananddeepsingh <singh.ananddeep@gmail.com>
2023-06-19 03:18:49 +0000

    Deploying to gh-pages from @ ananddeepsingh/React-emp-directory@dc445b13a506bee8b9666568f8b5d69901d23051 🚀

---
+++ precache-manifest.c65e5aba474b09d3b38cb5ae87b1ecbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb5e941977f32a99ca77211ec26886e",
+    "url": "/React-emp-directory/index.html"
+  },
+  {
+    "revision": "6e2a2dc27c50407522da",
+    "url": "/React-emp-directory/static/css/main.fde952d0.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/React-emp-directory/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6e2a2dc27c50407522da",
+    "url": "/React-emp-directory/static/js/main.7ad4300d.chunk.js"
+  },
+  {
+    "revision": "1a49d22e54d2b9b0c943",
+    "url": "/React-emp-directory/static/js/runtime~main.a046fd3c.js"
+  }
+]);
